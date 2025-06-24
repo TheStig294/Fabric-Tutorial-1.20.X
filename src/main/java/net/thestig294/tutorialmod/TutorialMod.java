b4c5d2f14dcfe3a5d580ddef0107598259478d78
The Stig <thestig294@gmail.com>
2025-06-24 12:42:33 +0930

    Tutorial Video 2 - Custom Item

--- src/main/java/net/thestig294/tutorialmod/TutorialMod.java
+++ src/main/java/net/thestig294/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.thestig294.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.thestig294.tutorialmod.item.ModItemGroups;
+import net.thestig294.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,8 +11,11 @@ public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+//	This is essentially like an autorun file from Lua,
+//	but instead of running files, you're running "main()" functions	from classes!
 	@Override
 	public void onInitialize() {
-
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
